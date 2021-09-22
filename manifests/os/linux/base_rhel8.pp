c9793b7d546ef1247d8a7d2773e1e69127799a28
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2021-09-22 22:42:18 +0800

    Update base_rhel8.pp

--- manifests/os/linux/base_rhel8.pp
+++ manifests/os/linux/base_rhel8.pp
@@ -2,7 +2,7 @@ class profiles::os::linux::base_rhel8 {
   include chrony
   include java::install
   include archive
-  include puppet_amazon_ssm_agent::amazon_ssm_agent
+  include amazon_ssm_agent
 # include sssd 
 # include pam
 # include motd
