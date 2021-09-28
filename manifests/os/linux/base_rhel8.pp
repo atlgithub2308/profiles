2f089fc8ed9600641fc6bc183d3ba3a510147da1
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2021-09-28 20:18:18 +0800

    Update base_rhel8.pp

--- manifests/os/linux/base_rhel8.pp
+++ manifests/os/linux/base_rhel8.pp
@@ -2,7 +2,7 @@ class profiles::os::linux::base_rhel8 {
   include chrony
   include java::install
   include archive
-  include amazonssmagent
+# include amazonssmagent
 # include sssd 
 # include pam
 # include motd
