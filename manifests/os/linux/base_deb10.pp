3d1d04db76b39972dcfcc5a5ae47910e14a1a0c6
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2021-09-30 14:56:38 +0800

    Update base_deb10.pp

--- manifests/os/linux/base_deb10.pp
+++ manifests/os/linux/base_deb10.pp
@@ -1,9 +1,21 @@
 class profiles::os::linux::base_deb10 {
-  include ssh
   include chrony
   
   user { ['sysad1', 'sysad2']:
     ensure => present,
   }
+  
+  package { 'openssl':
+    ensure => installed,
+  }
 
+  package { 'openssh':
+    ensure  => installed,
+    require => Package['openssl'],
+  }
+  
+  service { 'sshd':
+    ensure => running,
+    enable => true,
+  }
 }
